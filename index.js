d2bebb5e4a99c6c32975c1e378382158d52abf5a
Paul Donovan <paul.m.donovan@outlook.com>
2024-04-03 15:45:49 +1100

    changeSearch

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
 
+function renderOneProduct(product) {
+    // /build product 
+    let card = document.createElement('li')
+    card.className = 'card'
+    card.innerHTML = `
+<img src="${product.image}">
+<div class="content">
+<h4>${product.title}</h4>
+<p>${product.description}<p>
+</div>
+<div class="buttons">
+<button> Add to Cart</button>
+</div>
+`
+    document.querySelector('#product-list').appendChild(card)
+}
 
-
-
-fetch('http://localhost:3000/menswear')
-    .then(res => res.json())
-    .then((products) => {
-
-        const containerDiv = document.getElementById("container")
-
-        for (const product of products) {
-            const postTitle = document.createElement("h4");
-            postTitle.textContent = product.title;
-
-            const postDescription = document.createElement("p");
-            postDescription.textContent = product.description;
-
-            containerDiv.appendChild(postTitle);
-            containerDiv.appendChild(postDescription);
-
-            const productImage = document.createElement('img');
-            productImage.src = product.image;
-            console.log(productImage)
-            containerDiv.appendChild(productImage);
-
-        }
-    });
-
-const menswearData =
+// fetch request 
+function getAllProducts() {
     fetch('http://localhost:3000/menswear')
-        .then(res => console.log(res))
-// console.log('I have fetched')
-// this is a GET request 
+        .then(res => res.json())
+        .then(menswear => menswear.forEach(product => renderOneProduct(product)))
 
+}
+
+function initialise() {
+    getAllProducts()
+}
+initialise()
+
+document.getElementById("searchInput").addEventListener('input', function () {
+    const filter = this.value.toUpperCase();
+    const clothingList = document.getElementById("clothingList")
+    const items = clothingList.getElementsByTagName("li");
+    for (let i = 0; i < items.length; i++) {
+        if (text.toUpperCase().indexOf(filter) > -1) {
+            items.style.display = "";
+        } else {
+            items.style.display = "none";
+        }
+    }
+})
 
